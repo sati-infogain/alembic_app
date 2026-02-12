92d3d4be812fc5eeb07d2c592635c05f679703ee
sati-infogain <sathish.shanmugam@infogain.com>
2026-02-12 20:48:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
-import logging
-
 from fastapi import FastAPI
-
 from api.flows import router as flows_router
 from api.user_flows import router as user_flows_router
 from api.users import router as users_router
-
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
+import logging
+flows_router = aaa
 app = FastAPI(
-    title="User & Flow API",
-    version="1.0",
-    description="API for managing users, flows, and user-flow relationships."
+    title="User & Flow API" ,    version="1.0",      description="API for managing users, flows, and user-flow relationships."
 )
+import logging
 
 app.include_router(users_router)
-app.include_router(flows_router)
-app.include_router(user_flows_router)
+app.include_router(flows_router) app.include_router(user_flows_router)
