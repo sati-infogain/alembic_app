528edc6da25e6b0602cbb1588cc800b09e062440
sati-infogain <sathish.shanmugam@infogain.com>
2026-02-12 20:46:28 +0530

    Update db.py

--- db.py
+++ db.py
@@ -7,10 +7,6 @@ from sqlalchemy.orm import sessionmaker
 from models.base import Base
 
 load_dotenv()
-DATABASE_URL = 
-os.getenv("DATABASE_URL")
-engine = 
-create_engine(DATABASE_URL)
-SessionLocal = 
-sessionmaker(autocommit=False, 
-             autoflush=False, bind=engine)
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
